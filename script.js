0ca693a1315cd77e46aa80c5e08d79765fe8668e
Joshua Concepcion <joshuarconcepcion@gmail.com>
2025-04-03 19:26:45 -0700

    Initial draft of navigation bar and LinkedIn button

---
+++ script.js
@@ -0,0 +1,4 @@
+//LinkedIn Button Setup
+document.getElementById("linkedin-button").addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/joshua-concepcion-870354297/", "_blank");
+});
